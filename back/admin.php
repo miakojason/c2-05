25e8d329b81377311a4f818b2351c7f5766c0103
miakojason <miakojason@gmail.com>
2024-03-06 14:13:31 +0800

    back admin reg ok

--- back/admin.php
+++ back/admin.php
@@ -12,9 +12,9 @@
             foreach ($rows as $row) {
             ?>
                 <tr>
-                    <td><?=$row['acc'];?></td>
-                    <!-- <td><input style="border:0px" type="password" name="" value="<?=$row['acc'];?>"></td> -->
-                    <td><?=str_repeat("*",mb_strlen($row['acc']));?></td>
+                    <td><?= $row['acc']; ?></td>
+                    <!-- <td><input style="border:0px" type="password" name="" value="<?= $row['acc']; ?>"></td> -->
+                    <td><?= str_repeat("*", mb_strlen($row['acc'])); ?></td>
                     <td><input type="checkbox" name="del[]" value=""></td>
                 </tr>
             <?php
@@ -26,4 +26,61 @@
             <input type="reset" value="清空選取">
         </div>
     </form>
+    <h1>新增會員</h1>
+    <span style="color:red">*請設定您要註冊的帳號及密碼（最長 12 個字元）</span>
+    <table>
+        <tr>
+            <td class="clo">Step1:登入帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step2:登入密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step3:再次確認密碼</td>
+            <td><input type="password" name="pw2" id="pw2"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step4:信箱(忘記密碼時使用)</td>
+            <td><input type="email" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="註冊" onclick="reg()"><input type="reset" value="清除" onclick="clean()"></td>
+            <td></td>
+        </tr>
+    </table>
 </fieldset>
+<script>
+    function clean() {
+        $("input[type='text'],input[type='password'],input[type='radio']").val("")
+    }
+
+    function reg() {
+        let user = {
+            acc: $("#acc").val(),
+            pw: $("#pw").val(),
+            pw2: $("#pw2").val(),
+            email: $("#email").val()
+        }
+        if (user.acc != "" && user.pw != "" && user.pw2 != "" && user.email != "") {
+            if (user.pw == user.pw2) {
+                $.post("./api/chk_acc.php", {
+                    acc: user.acc
+                }, (res) => {
+                    if (parseInt(res) == 1) {
+                        alert("帳號重複")
+                    } else {
+                        $.post("./api/reg.php", user, () => {
+                            location.reload()
+                        })
+                    }
+                })
+            } else {
+                alert("密碼錯誤")
+            }
+        } else {
+            alert("不可空白")
+        }
+    }
+</script>
