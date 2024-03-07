f1aa1db1fc0dff4c3e5bc6a0258e5afc27d5b668
miakojason <miakojason@gmail.com>
2024-03-07 11:56:36 +0800

    fron result 差一半

--- front/result.php
+++ front/result.php
@@ -1,5 +1,17 @@
 <?php $row = $Que->find($_GET['id']); ?>
 <fieldset>
-<legend>目前位置:首頁>問卷調查><?= $row['text']; ?></legend>
+    <legend>目前位置:首頁>問卷調查><?= $row['text']; ?></legend>
     <h3><?= $row['text']; ?></h3>
+    <?php
+    $rows = $Que->all(['subject_id' => $_GET['id']]);
+    foreach ($rows as $row) {
+    ?>
+        <div style="display:flex;">
+            <div style="width:50%"><?=$row['text'];?></div>
+            <div class="clo" style="width:40%"></div>
+            <div style="width:10%"><?=$row['vote'];?>票(%)</div>
+        </div>
+    <?php
+    }
+    ?>
 </fieldset>
